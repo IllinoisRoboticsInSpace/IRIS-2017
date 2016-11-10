b17eea2df248ebab49de72e40966a69b662b3b47
Andres Rodriguez Reina <andres.r.reina@gmail.com>
2016-11-09 18:12:36 -0600

    New folder structure Remove old code

--- wIRIS/build/obstacle_detection/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/iris/wIRIS/src/obstacle_detection
-# Build directory: /home/iris/wIRIS/build/obstacle_detection
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- wIRIS/build/obstacle_detection/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/iris/wIRIS/src/obstacle_detection/include".split(';') if "/home/iris/wIRIS/src/obstacle_detection/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "pcl_conversions;pcl_ros;roscpp;sensor_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lobstacle_detection".split(';') if "-lobstacle_detection" != "" else []
-PROJECT_NAME = "obstacle_detection"
-PROJECT_SPACE_DIR = "/home/iris/wIRIS/devel"
-PROJECT_VERSION = "0.0.0"
