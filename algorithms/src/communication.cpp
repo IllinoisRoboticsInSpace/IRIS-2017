08259f52c76d3a9667eb6df2f3cd2fae49109984
Andres Rodriguez Reina <andres.r.reina@gmail.com>
2017-05-25 21:56:24 -0500

    FSM testing night

--- algorithms/src/communication.cpp
+++ algorithms/src/communication.cpp
@@ -28,6 +28,7 @@ void* communication_motor(void * unused)
         locate_motor desired = get_desired_motor();
         char c[100];
         sprintf(c, "!G 1 %d_!G 2 %d_\n",desired.motor_left,desired.motor_right);
+        printf("%s",c);
         motor_serial.send(c, strlen(c));
         sleep(.1);
 
@@ -43,6 +44,7 @@ void* communication_actuators(void * unused)
         locate_actuator desired = get_desired_actuator();
         char c[100];
         sprintf(c,"!%d,%d,%d,%d#!\n",desired.collection,desired.bin,desired.webcam,desired_angle);
+        printf("%s",c);
         actuator_serial.send(c, strlen(c));
         sleep(.1);
 
